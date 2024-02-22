dcad23b110eb5e6ee1954eaf4538b7bd53ef850f
abubakarasgharali <160823088+abubakarasgharali@users.noreply.github.com>
2024-02-22 11:31:04 +0500

    Add files via upload

---
+++ abubakar[1].c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int k,j;
+clrscr();
+for(k=2;k<=555;k++)
+{
+for(j=0;j<k;j++)
+printf("abubakkkarhello bro apa all ha bhia");
+printf("completely process");
+    printf("%d");
+    
+   printf("/n");
+   }
+   getch();
+   }
